8ad087c72b0085213743467afb410912f8c824a8
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-05-16 21:42:45 +0100

    rename styles and add defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        primary: "var(--theme-primary)",
-        secondary: "var(--theme-secondary)",
-        accent: "var(--theme-accent)",
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				primary: "var(--theme-primary)",
+				secondary: "var(--theme-secondary)",
+				accent: "var(--theme-accent)",
+			},
+			typography: (theme) => ({
+				cactus: {
+					css: {
+						"--tw-prose-body": "var(--theme-text)",
+						"--tw-prose-headings": "var(--theme-text)",
+						"--tw-prose-links": "var(--theme-text)",
+					},
+				},
+				DEFAULT: {
+					css: {
+						a: {
+							"&:hover": {
+								textDecorationColor: theme("colors.secondary"),
+								textDecorationStyle: "double",
+							},
+						},
+					},
+				},
+			}),
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
 };
