4370eaac1e363e0c36d2aa9eb5c19dcc28de380d
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-05-17 19:37:37 +0100

    add solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,27 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
-  markdown: {
-    remarkPlugins: ["remark-code-titles"],
-    rehypePlugins: [
-      "rehype-slug",
-      ["rehype-autolink-headings", { behavior: "prepend" }],
-      [
-        "rehype-toc",
-        {
-          /* https://github.com/JS-DevTools/rehype-toc */
-        },
-      ],
-    ],
-    shikiConfig: {
-      theme: "dracula",
-      wrap: true,
-    },
-  },
-  site: "https://www.astro-cactus.netlify.app",
-  integrations: [tailwind(), sitemap()],
+	markdown: {
+		remarkPlugins: ["remark-code-titles"],
+		rehypePlugins: [
+			"rehype-slug",
+			[
+				"rehype-autolink-headings",
+				{
+					behavior: "prepend",
+				},
+			],
+		],
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	site: "https://www.astro-cactus.netlify.app",
+	integrations: [tailwind(), sitemap(), solid()],
 });
