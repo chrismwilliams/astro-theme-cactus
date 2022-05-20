e5d6f086a6802d80fd72959aaa824ed89a3db605
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-05-20 12:17:37 +0100

    swap the svg around

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -36,7 +36,7 @@ export default function ThemeToggle(props) {
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
 			>
-				{theme() ? (theme() === "light" ? (
+				{theme() ? (theme() === "dark" ? (
 					<>
 						<path
 							d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18Z"
