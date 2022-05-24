dbf7d8376cebfde485c6d66e022ca04a07c24740
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-05-24 14:20:45 +0100

    add theme colour & styles

--- src/util.ts
+++ src/util.ts
@@ -1,26 +1,33 @@
 export function sortMDByDate(files) {
-  return files.sort((a, b) => new Date(b.frontmatter.publishDate).valueOf() - new Date(a.frontmatter.publishDate).valueOf());
+	return files.sort(
+		(a, b) =>
+			new Date(b.frontmatter.publishDate).valueOf() -
+			new Date(a.frontmatter.publishDate).valueOf()
+	);
 }
 
 export function slugsFromFrontmatter(files): string[] {
-  return files.map((file) => file.url);
+	return files.map((file) => file.url);
 }
 
-export function getPreviousAndNextSlugs(currentSlug: string, slugArr: string[]) {
-  let index = slugArr.indexOf(currentSlug);
-  return {
-    prev: slugArr[index - 1] ?? null,
-    next: slugArr[index + 1] ?? null
-  }
+export function getPreviousAndNextSlugs(
+	currentSlug: string,
+	slugArr: string[]
+) {
+	let index = slugArr.indexOf(currentSlug);
+	return {
+		prev: slugArr[index - 1] ?? null,
+		next: slugArr[index + 1] ?? null,
+	};
 }
 
 export function getInitialTheme() {
-  const isDark = getDocumentClassList().contains('dark')
-  const storageTheme = localStorage.getItem('theme')
-  if (storageTheme) return storageTheme
-  return isDark ? 'dark' : 'light'
+	const isDark = getDocumentClassList().contains("dark");
+	const storageTheme = localStorage.getItem("theme");
+	if (storageTheme) return storageTheme;
+	return isDark ? "dark" : "light";
 }
 
 export function getDocumentClassList() {
-  return document.documentElement.classList;
+	return document.documentElement.classList;
 }
