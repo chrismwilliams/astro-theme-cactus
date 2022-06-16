e9c66f28c73fa8e4f784e6778fe5188be3802920
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-06-16 15:07:56 +0100

    add additional seo, icons, expose meta options

---
+++ src/site-meta.config.ts
@@ -0,0 +1,11 @@
+const siteMeta = {
+  title: "Astro Theme Cactus",
+  description: "An opinionated starter theme for Astro",
+  lang: "en-GB",
+  ogLocale: "en_GB",
+  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+  themeColorLight: "#fafafa",
+  themeColorDark: "#1d1f21",
+}
+
+export default siteMeta;
--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,9 @@ export type Theme = 'light' | 'dark';
 export interface IElement {
   readonly as?: keyof HTMLElementTagNameMap;
 }
+
+export type SiteMeta = {
+  title: string,
+  description?: string,
+  image?: string
+}
