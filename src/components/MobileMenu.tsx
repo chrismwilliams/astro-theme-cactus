50a29d2584161e56e02ae64bbc9a0cfbbbf9f7ed
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-06-23 15:15:39 +0100

    remove blog header and slots

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -11,7 +11,7 @@ export default function Header() {
 			onClick={toggleMenuOpen}
 		>
 			<svg
-				class="w-6 h-6"
+				class="h-6 w-6"
 				fill="currentColor"
 				viewBox="0 0 20 20"
 				xmlns="http://www.w3.org/2000/svg"
