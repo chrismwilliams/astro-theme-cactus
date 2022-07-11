b17ecd1a79764dace35b546c866d5eb1ee007137
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-07-11 15:36:39 +0100

    util functions to handle missing tags

--- src/util.ts
+++ src/util.ts
@@ -1,51 +1,41 @@
-export function sortMDByDate(files) {
-	return files.sort(
+import type { IPost } from "./types";
+
+export function sortMDByDate(posts: IPost[] = []) {
+	return posts.sort(
 		(a, b) =>
 			new Date(b.frontmatter.publishDate).valueOf() -
 			new Date(a.frontmatter.publishDate).valueOf()
 	);
 }
 
-export function blogLinksFromFrontmatter(
-	files
-): { title: string; url: string }[] {
-	return files.map((file) => {
-		return {
-			title: file.frontmatter.title,
-			url: file.url,
-		};
-	});
-}
-
-// This function expects the @arg postArr to be sorted by sortMDByDate()
+// This function expects the @arg posts to be sorted by sortMDByDate()
 export function getPreviousAndNextPosts(
 	currentSlug: string,
-	postArr: { title: string; url: string }[]
+	posts: IPost[]
 ) {
-	const index = postArr.findIndex(({ url }) => url === currentSlug);
+	const index = posts.findIndex(({ url }) => url === currentSlug)
 	return {
-		prev: postArr[index + 1] ?? null,
-		next: postArr[index - 1] ?? null,
+		prev: posts[index + 1] ?? null,
+		next: posts[index - 1] ?? null,
 	};
 }
 
-export function getAllTagsFromPosts(posts, withCount = false) {
-	if (withCount) {
-		return posts.reduce((prev, post) => {
-			const currTags = { ...prev };
-			const postTags = post.frontmatter.tags;
-			postTags.forEach(function (tag) {
-				currTags[tag] = (currTags[tag] || 0) + 1;
-			});
-			return currTags;
-		}, {});
-	}
-	const allTags = new Set();
+export function getAllTags(posts: IPost[] = []) {
+	const allTags = new Set<string>();
 	posts.forEach((post) => {
-		post.frontmatter.tags &&
-			post.frontmatter.tags.map((tag) => allTags.add(tag.toLowerCase()));
+		post.frontmatter.tags?.map((tag) => allTags.add(tag.toLowerCase()))
 	});
 	return [...allTags];
+}
+
+export function getAllTagsWithCount(posts: IPost[] = []): { [key: string]: number } {
+	return posts.reduce((prev, post) => {
+		const currTags = { ...prev };
+		post.frontmatter.tags?.forEach(function (tag) {
+			currTags[tag] = (currTags[tag] || 0) + 1;
+		});
+		return currTags;
+	}, {});
 }
 
 export function toggleClass(element: HTMLElement, className: string) {
