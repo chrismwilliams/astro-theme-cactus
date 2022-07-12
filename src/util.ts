751836070a43af4329a5652cbbe4c2abf5140d5b
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-07-12 12:08:08 +0100

    Revert "Merge branch 'update/post-layout-imports' into main"
    This reverts commit 0eadf82c048aab3b49a6462a8f5a273f03688494.

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,5 @@
-import type { IPost } from "./types";
-
-export function sortMDByDate(posts: IPost[] = []) {
-	return posts.sort(
+export function sortMDByDate(files) {
+	return files.sort(
 		(a, b) =>
 			new Date(b.frontmatter.publishDate).valueOf() -
 			new Date(a.frontmatter.publishDate).valueOf()
@@ -9,26 +7,35 @@ export function sortMDByDate(posts: IPost[] = []) {
 }
 
 // This function expects the @arg posts to be sorted by sortMDByDate()
-export function getPreviousAndNextPosts(
-	currentSlug: string,
-	posts: IPost[]
-) {
-	const index = posts.findIndex(({ url }) => url === currentSlug)
+export function getPreviousAndNextPosts(currentSlug: string, posts: IPost[]) {
+	const index = posts.findIndex(({ url }) => url === currentSlug);
 	return {
-		prev: posts[index + 1] ?? null,
-		next: posts[index - 1] ?? null,
+		prev: postArr[index + 1] ?? null,
+		next: postArr[index - 1] ?? null,
 	};
 }
 
-export function getAllTags(posts: IPost[] = []) {
-	const allTags = new Set<string>();
+export function getAllTagsFromPosts(posts, withCount = false) {
+	if (withCount) {
+		return posts.reduce((prev, post) => {
+			const currTags = { ...prev };
+			const postTags = post.frontmatter.tags;
+			postTags.forEach(function (tag) {
+				currTags[tag] = (currTags[tag] || 0) + 1;
+			});
+			return currTags;
+		}, {});
+	}
+	const allTags = new Set();
 	posts.forEach((post) => {
-		post.frontmatter.tags?.map((tag) => allTags.add(tag.toLowerCase()))
+		post.frontmatter.tags?.map((tag) => allTags.add(tag.toLowerCase()));
 	});
 	return [...allTags];
 }
 
-export function getAllTagsWithCount(posts: IPost[] = []): { [key: string]: number } {
+export function getAllTagsWithCount(posts: IPost[] = []): {
+	[key: string]: number;
+} {
 	return posts.reduce((prev, post) => {
 		const currTags = { ...prev };
 		post.frontmatter.tags?.forEach(function (tag) {
@@ -43,5 +50,20 @@ export function toggleClass(element: HTMLElement, className: string) {
 }
 
 export function elementHasClass(element: HTMLElement, className: string) {
-	return element.classList.contains(className)
+	return element.classList.contains(className);
+}
+
+
+export function getLocaleTime(
+	date: number | Date,
+	options: Intl.DateTimeFormatOptions = {},
+	locale: string | string[] = "en-GB"
+) {
+	const formatOptions: Intl.DateTimeFormatOptions = {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+		...options,
+	};
+	return new Intl.DateTimeFormat(locale, formatOptions).format(date);
 }
