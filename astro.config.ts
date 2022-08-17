c5fa99707b5bf7d7a79abfcfde64520625a9e7a9
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-08-17 16:04:47 +0100

    astro config and pkg changes

---
+++ astro.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	site: "https://www.astro-theme-cactus.netlify.app",
+	integrations: [
+		mdx({}),
+		tailwind({
+			config: { applyBaseStyles: false },
+		}),
+		image(),
+		sitemap(),
+	],
+});
