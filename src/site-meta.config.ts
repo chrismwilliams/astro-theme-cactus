225a3dad4b771d19ec6702c06da3b5f1c154a726
x7md <x7mdNet@gmail.com>
2022-09-20 23:01:32 +0300

    fix hash

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,8 +1,8 @@
 const siteMeta = {
   title: "Astro Theme Cactus",
   description: "An opinionated starter theme for Astro",
-  lang: "en-GB",
-  ogLocale: "en_GB",
+  lang: "ar",
+  ogLocale: "ar_SA",
   githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
