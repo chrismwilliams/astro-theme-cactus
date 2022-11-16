98c79038df76a34c059b7784c60bec79fe14d1ed
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-11-16 16:17:24 +0000

    cleanup colours, disable unused tailwind plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
 	darkMode: "class",
 	corePlugins: {
-		// disable aspect ratio as per: @tailwindcss/aspect-ratio
+		// disable aspect ratio as per docs -> @tailwindcss/aspect-ratio
 		aspectRatio: false,
 		// disable some core plugins as they are included in the css, even when unused
 		touchAction: false,
@@ -20,11 +20,11 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				bgColor: "hsl(var(--theme-bg) / <alpha-value>)",
-				textColor: "hsl(var(--theme-text) / <alpha-value>)",
-				link: "hsl(var(--theme-link) / <alpha-value>)",
-				accent: "hsl(var(--theme-accent) / <alpha-value>)",
-				"accent-2": "hsl(var(--theme-accent-2) / <alpha-value>)",
+				bgColor: "var(--theme-bg)",
+				textColor: "var(--theme-text)",
+				link: "var(--theme-link)",
+				accent: "var(--theme-accent)",
+				"accent-2": "var(--theme-accent-2)",
 			},
 			fontFamily: {
 				// Add any custom fonts here
