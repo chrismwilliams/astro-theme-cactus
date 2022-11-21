f9c23f8c1faba3c54545da7657052d8ddffb7867
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-11-21 11:56:49 +0000

    add astro prefetch integration

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,9 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
 
+// https://astro.build/config
 export default defineConfig({
 	site: "https://www.astro-theme-cactus.netlify.app",
 	markdown: {
@@ -15,11 +17,14 @@ export default defineConfig({
 	integrations: [
 		mdx({}),
 		tailwind({
-			config: { applyBaseStyles: false },
+			config: {
+				applyBaseStyles: false,
+			},
 		}),
 		image({
 			serviceEntryPoint: "@astrojs/image/sharp",
 		}),
 		sitemap(),
+		prefetch(),
 	],
 });
