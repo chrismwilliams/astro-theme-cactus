b32476d9e4797d65142e8d2f8e04973f65edf077
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-11-26 11:35:02 +0000

    install latest tailwind prettier plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,7 +5,11 @@ module.exports = {
 	singleQuote: false,
 	tabWidth: 2,
 	useTabs: true,
-	plugins: [require("prettier-plugin-astro")],
+	plugins: [
+		require("prettier-plugin-astro"),
+		require("prettier-plugin-tailwindcss") /* Must come last */,
+	],
+	pluginSearchDirs: false,
 	overrides: [
 		{
 			files: "**/*astro",
