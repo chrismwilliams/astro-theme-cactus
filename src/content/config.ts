46f3c32ab04aaa2532431cad3f8e471f45b2ebac
Chris Williams <chrismwilliams@users.noreply.github.com>
2023-01-25 15:38:21 +0000

    fix astro v2 issues

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
 import { z, defineCollection } from "astro:content";
 
 const post = defineCollection({
-	schema: {
+	schema: z.object({
 		title: z.string().max(60),
 		description: z.string().min(50).max(160),
 		publishDate: z.string().transform((str) => new Date(str)),
 		tags: z.array(z.string()).default([]),
 		ogImage: z.string().optional(),
-	},
+	}),
 });
 
 export const collections = { post };
---
+++ src/env.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="@astrojs/image/client" />
+/// <reference path="../.astro/types.d.ts" />
