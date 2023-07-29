95dc039bb52306852800a90070c8e5f11459f288
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2023-07-29 16:01:18 +0200

    Update constants.ts

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -11,12 +11,16 @@ export const MENU_LINKS = [
 		title: "Blog",
 		path: "/posts/",
 	},
+	{
+		title: "FAQ",
+		path: "/faq/",
+	},  
 ];
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
-	discord: "https://github.com/chrismwilliams/astro-cactus",
+	twitter: "https://x.com/#",
+	discord: "https://discord.com/#",
 	email: "me@example.com",
 };
