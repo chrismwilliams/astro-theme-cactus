8eaa20e5c869d2d405d99cc52544751b85b5adff
Chris Williams <chrismwilliams@users.noreply.github.com>
2023-09-04 08:18:21 +0100

    tags with count returns a sorted array tuple

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,6 +1,6 @@
 import type { CollectionEntry } from "astro:content";
 
-export function sortMDByDate(posts: CollectionEntry<"post">[]) {
+export function sortMDByDate(posts: Array<CollectionEntry<"post">>) {
 	return posts.sort((a, b) => {
 		const aDate = new Date(a.data.updatedDate ?? a.data.publishDate).valueOf();
 		const bDate = new Date(b.data.updatedDate ?? b.data.publishDate).valueOf();
@@ -8,17 +8,21 @@ export function sortMDByDate(posts: CollectionEntry<"post">[]) {
 	});
 }
 
-export function getUniqueTags(posts: CollectionEntry<"post">[]) {
-	const tags = posts.flatMap((post) => [...post.data.tags]);
-  return [...new Set(tags)];
+export function getAllTags(posts: Array<CollectionEntry<"post">>) {
+	return posts.flatMap((post) => [...post.data.tags]);
 }
 
-export function getUniqueTagsWithCount(posts: CollectionEntry<"post">[]): Record<string, number> {
-	return posts.reduce((acc, post) => {
-		const runningTags: Record<string, number> = { ...acc };
-		post.data.tags.forEach((tag) => {
-			runningTags[tag] = (runningTags[tag] || 0) + 1;
-		});
-		return runningTags;
-	}, {});
+export function getUniqueTags(posts: Array<CollectionEntry<"post">>) {
+	return [...new Set(getAllTags(posts))];
+}
+
+export function getUniqueTagsWithCount(
+	posts: Array<CollectionEntry<"post">>,
+): Array<[string, number]> {
+	return [
+		...getAllTags(posts).reduce(
+			(acc, t) => acc.set(t, (acc.get(t) || 0) + 1),
+			new Map<string, number>(),
+		),
+	].sort((a, b) => b[1] - a[1]);
 }
