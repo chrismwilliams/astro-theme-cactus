f34810d7c05e9c0144b08bb9363acc9b42644ac8
Chris Williams <chrismwilliams@users.noreply.github.com>
2023-10-03 14:27:45 +0100

    Add webmention types

--- src/types.ts
+++ src/types.ts
@@ -27,3 +27,59 @@ export type SiteMeta = {
 	ogImage?: string | undefined;
 	articleDate?: string | undefined;
 };
+
+/** Webmentions */
+export type WebmentionsFeed = {
+	type: string;
+	name: string;
+	children: WebmentionsChildren[];
+};
+
+export type WebmentionsCache = {
+	lastFetched: string | null;
+	children: WebmentionsChildren[];
+};
+
+export type WebmentionsChildren = {
+	type: string;
+	author: Author;
+	url: string;
+	published?: string | null;
+	"wm-received": string;
+	"wm-id": number;
+	"wm-source": string;
+	"wm-target": string;
+	"wm-protocol": string;
+	syndication?: string[] | null;
+	content?: Content | null;
+	"mention-of": string;
+	"wm-property": string;
+	"wm-private": boolean;
+	rels?: Rels | null;
+	name?: string | null;
+	photo?: string[] | null;
+	summary?: Summary | null;
+};
+
+export type Author = {
+	type: string;
+	name: string;
+	photo: string;
+	url: string;
+};
+
+export type Content = {
+	"content-type": string;
+	value: string;
+	html: string;
+	text: string;
+};
+
+export type Rels = {
+	canonical: string;
+};
+
+export type Summary = {
+	"content-type": string;
+	value: string;
+};
