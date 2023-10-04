4db5d7906958e66682bdf32ac11d06c0df03159b
Chris Williams <chrismwilliams@users.noreply.github.com>
2023-10-04 15:35:14 +0100

    add webmentions fetching

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export { elementHasClass, toggleClass } from "./domElement";
 export { getAllPosts, sortMDByDate, getUniqueTags, getUniqueTagsWithCount } from "./post";
 export { generateToc } from "./generateToc";
 export type { TocItem } from "./generateToc";
+export { getWebmentionsForUrl } from "./webmentions";
