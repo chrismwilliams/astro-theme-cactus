4ddf282e071bdf6abe7d921b4c1d4fbd282cc132
Chris Williams <chrismwilliams@users.noreply.github.com>
2023-10-09 13:26:11 +0100

    add helper theme fn

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export { getFormattedDate } from "./date";
-export { elementHasClass, toggleClass } from "./domElement";
+export { elementHasClass, toggleClass, rootHasDarkClass } from "./domElement";
 export { getAllPosts, sortMDByDate, getUniqueTags, getUniqueTagsWithCount } from "./post";
 export { generateToc } from "./generateToc";
 export type { TocItem } from "./generateToc";
