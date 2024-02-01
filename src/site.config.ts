b4200a8cb81c8de0081ea7ba33caff0e3498f841
Henry Ha <henryha993@gmail.com>
2024-02-01 11:23:28 +0000

    Changed social image and titles

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Henry Ha",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Heny",
+	title: "heny",
 	// Meta property used as the default description meta property
 	description: "My personal website for games development and ramblings.",
 	// HTML lang property, found in src/layouts/Base.astro L:18
