acffe8bf7ec75d75a255a7e59f6a0d16ffe5d163
Chris Williams <chrismwilliams@users.noreply.github.com>
2024-02-12 10:21:15 +0000

    update config and tooling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,43 +1,68 @@
 /** @type {import("@types/eslint").Linter.Config} */
 module.exports = {
-	ignorePatterns: ["node_modules", "dist"],
-	root: true,
 	env: {
 		node: true,
 	},
-	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint", "prettier"],
 	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
-		"plugin:prettier/recommended",
-		"prettier",
 		"plugin:astro/recommended",
 		"plugin:astro/jsx-a11y-recommended",
+		"plugin:perfectionist/recommended-natural",
+		"plugin:regexp/recommended",
+		"plugin:typescript-sort-keys/recommended",
 	],
-	rules: {
-		"@typescript-eslint/no-var-requires": "warn",
-		"@typescript-eslint/no-unused-vars": [
-			"warn",
-			{ varsIgnorePattern: "Props", ignoreRestSiblings: true },
-		],
-	},
+	ignorePatterns: ["node_modules", "dist"],
 	overrides: [
 		{
+			extends: ["plugin:@typescript-eslint/strict", "plugin:@typescript-eslint/stylistic"],
 			files: ["*.astro"],
 			parser: "astro-eslint-parser",
 			parserOptions: {
-				parser: "@typescript-eslint/parser",
 				extraFileExtensions: [".astro"],
+				parser: "@typescript-eslint/parser",
 			},
 			rules: {
 				"astro/jsx-a11y/no-redundant-roles": [
 					"error",
 					{
+						ol: ["list"],
 						ul: ["list"],
 					},
 				],
 			},
 		},
+		{
+			extends: [
+				"plugin:@typescript-eslint/strict-type-checked",
+				"plugin:@typescript-eslint/stylistic-type-checked",
+			],
+			files: ["*.ts", "*.tsx"],
+			rules: {
+				"deprecation/deprecation": "error",
+			},
+		},
+		{
+			extends: ["plugin:markdown/recommended"],
+			files: ["**/*.md"],
+			processor: "markdown/markdown",
+		},
 	],
+	parser: "@typescript-eslint/parser",
+	plugins: [
+		"@typescript-eslint",
+		"astro",
+		"deprecation",
+		"perfectionist",
+		"regexp",
+		"typescript-sort-keys",
+	],
+	root: true,
+	rules: {
+		"@typescript-eslint/no-unused-vars": [
+			"warn",
+			{ ignoreRestSiblings: true, varsIgnorePattern: "Props" },
+		],
+		"@typescript-eslint/no-var-requires": "warn",
+	},
 };
