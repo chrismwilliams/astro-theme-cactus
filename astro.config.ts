01771daafff0ee85080b406904a054a4b4cb553a
Henry Ha <henryha993@gmail.com>
2024-03-12 16:43:11 +0000

    Updated domain

--- astro.config.ts
+++ astro.config.ts
@@ -10,7 +10,7 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "www.heny.quest",
+	site: "https://heny.quest",
 	//base: "/HenryHa993.github.io",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
