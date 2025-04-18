928297b3ad11f8597ad64785f8829859d9fb94de
MESSIUINIMI <117837071+MESSIUINIMI@users.noreply.github.com>
2025-04-18 22:01:21 +0800

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "MESSIUINIMI",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: "en-GB",
@@ -20,9 +20,9 @@ export const siteConfig: SiteConfig = {
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "en_GB",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "Astro Theme Cactus",
+	title: "MESSIUINIMI",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	url: "https://astro-cactus.chriswilliams.dev/",
+	url: "https://messiuinimi.netlify.app/",
 };
 
 // Used to generate links in both the Header & Footer.
