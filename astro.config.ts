58436c985f487f99685cd8c450cb01d6c16a4952
Ben Handzo <ben.handzo@gmail.com>
2025-09-11 15:58:34 -0700

    Switch to server output mode for Vercel deployment

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    output: "hybrid",
+    output: "server",
     adapter: vercel(),
     site: siteConfig.url,
     image: {
